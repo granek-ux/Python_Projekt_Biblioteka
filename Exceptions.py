f4c8f1f34b19ec015fffdd7570629b8b39ee06bf
s31418 <kuba.balina@gmail.com>
2025-05-09 13:40:24 +0200

    Exception added ReaderNotFound

---
+++ Exceptions.py
@@ -0,0 +1,7 @@
+# propozycja aby wszytkie wyjątki w 1 miescju
+# albo tym pliku
+#albo w róznych plikach ale w 1 folderze
+
+class ReaderNotFound(Exception):
+    # kiedy nie znajdzie się czytlenika o podanym id
+    pass
