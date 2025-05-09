537233d5f6055d31fff994c0147e74d0048ffd16
s31418 <kuba.balina@gmail.com>
2025-05-09 13:39:56 +0200

    first version of history in book

--- Book.py
+++ Book.py
@@ -2,7 +2,7 @@ class Book:
     # działa jak static w java to nextID
     nextId = 1
 
-    def __init__(self, title: str, author: str, isbn: int, pages: int)->None:
+    def __init__(self, title: str, author: str, isbn: int, pages: int) -> None:
         self._id = Book.nextId
         Book.nextId += 1
         self.title = title
