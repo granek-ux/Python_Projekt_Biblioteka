805da565e80d5107a06eed71855221da4ca9474a
Jakub Balinski <j.balinski@wp.pl>
2025-05-09 22:09:03 +0200

    UserInterface COLORS!!!!!!!!!!!!!!

---
+++ main.py
@@ -0,0 +1,23 @@
+def interface()-> None:
+    print('\033[31mWitaj w bibliotece\033[0m'.center(50,' '))
+    # ewentualnie biblioteka dla łatwiejszej składni
+    #bo ta domyślna to mało wygodna
+    print('Możesz w miej zrobić:')
+    print('\033[32m1. Dodać Czytelnika\033[0m')
+    print('\033[31m2. Usunąć czytelnika')
+    print('3. Wyjść ')
+
+
+
+
+
+    code = (int) (input())
+    match(code):
+        case 1:
+            print('\033[31mDodaleś Czytelnika\033[0m')
+        case 2:
+            print('Usunałes')
+        case 3:
+            print("Do Widzenia".center(50,' '))
+
+interface()
