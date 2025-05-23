555ac56ba7cd6d9532ca82cd9a9e50095a4ec58d
Jakub Balinski <j.balinski@wp.pl>
2025-05-23 22:09:33 +0200

    pomysły na Exception

--- Exceptions.py
+++ Exceptions.py
@@ -21,3 +21,17 @@ class BookAlreadyTaken(Exception):
 class NoBookReserved(Exception):
     #kiedy nie ma zarezerwowanej książki
     pass
+
+#todo
+# wiecej wyjątków!!!!!!!!
+# z gpt do przemyślenia
+# Możesz dodać następujące wyjątki, aby łącznie było ich co najmniej 10:
+# ReaderAlreadyExists – gdy próbujesz dodać czytelnika, który już istnieje.
+# BookAlreadyExists – gdy próbujesz dodać książkę, która już jest w bazie.
+# MaxBooksLimitReached – gdy czytelnik przekroczył limit wypożyczonych książek.
+# BookNotAvailable – gdy książka jest już wypożyczona przez kogoś innego.
+# ReservationExpired – gdy rezerwacja książki wygasła.
+# InvalidBookId – gdy podano nieprawidłowy identyfikator książki.
+# InvalidReaderId – gdy podano nieprawidłowy identyfikator czytelnika.
+# BookReturnLate – gdy książka została zwrócona po terminie.
+# ReservationNotAllowed – gdy rezerwacja nie jest możliwa z powodu ograniczeń.
